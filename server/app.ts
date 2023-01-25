3df164bd0dbb68bcd36f79d41c38c48201f95682
salt-dev <salt-dev@salt-devs-MacBook-Air-9.local>
2023-01-25 11:05:58 +0100

    implement filter in route

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,13 @@
 import express, { Request, Response, Application } from 'express';
 import * as db from './db.json';
 
+interface Filtered {
+  name: string,
+  type: string,
+  storage: number,
+  id: string,
+}
+
 const app: Application = express();
 app.use(express.json());
 
@@ -8,12 +15,39 @@ app.get('/api/hello', (_req: Request, res: Response) => {
   return res
     .status(200)
     .json({ test: 'it works as it should' });
-});
+}); ''
 
-app.get('/api/milk', (_req: Request, res: Response) => {
-  return res
-    .status(200)
-    .json({ db: db });
-});
+app.get('/api/milk', (req: Request, res: Response) => {
+  try {
+    const filter = req.query.filter as string;
 
+    let response = db;
+
+    if (filter) {
+      const adjustedFilter = filter.replace('-', ' ').toLowerCase();
+      const dbItems = db.results.map(item => item);
+      const filtered = dbItems.filter(item => item.type.toLowerCase() === adjustedFilter);
+      //@ts-ignore
+      response = filtered as Filtered;
+    }
+
+    res
+      .status(200)
+      .json({ db: response });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ error: true, message: 'Server error' })
+  }
+});
+// app.get('/api/milk', (_req: Request, res: Response) => {
+//   return res
+//     .status(200)
+//     .json({ db: db });
+// });
+// const page = parseInt(req.query.page as string) - 1 || 0;
+// const limit = parseInt(req.query.limit as string) || 5;
+// const searchQuery = req.query.search as string || '';
+// let  sort = req.query.sort as string || ''; maybe sort by storage
 export default app;
